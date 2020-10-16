c81d9d3c6c8defea0450ff4254d9e72e12a05adb
fufu717 <hiroyuki.kawai777@gmail.com>
2020-10-16 12:48:17 +0900

    ユーザー新規登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,17 @@
 class User < ApplicationRecord
+  
+   validates :nickname,:first_name, :last_name, :birth_day, presence: true
+
+   validates :password, presence: true,
+             format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]{6,100}+\z/i, message:"パスワードは英数をそれぞれ１文字以上使ってください" }
+
+   validates :first_name_kana, :last_name_kana, presence: true, 
+             format: { with: /\A([ァ-ン]|ー)+\z/, message: "is must NOT contain any other characters than alphanumerics." }
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+ 
+ 
 end
