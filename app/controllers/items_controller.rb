db09ca497f6332ab98c803bb4c52dd3c83964afb
fufu717 <hiroyuki.kawai777@gmail.com>
2020-10-19 16:50:22 +0900

    commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,15 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def def new
+  end
+  
+  def create
+    
+  end
+  
+  def item_params
+    params.require(:item).permit(:name,:price,:description,:image).merge(user_id: current_user.id)
+  end
 end
