738037be76a06fc9035978b99e0a004d2d9c2b3c
fufu717 <hiroyuki.kawai777@gmail.com>
2020-10-21 18:30:54 +0900

    マイグレーションリセット

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_19_020328) do
+ActiveRecord::Schema.define(version: 2020_10_21_022752) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -35,7 +35,6 @@ ActiveRecord::Schema.define(version: 2020_10_19_020328) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
-    t.bigint "user_id"
     t.integer "category_id", null: false
     t.integer "condition_id", null: false
     t.integer "postage_id", null: false
@@ -43,6 +42,7 @@ ActiveRecord::Schema.define(version: 2020_10_19_020328) do
     t.integer "prefecture_id", null: false
     t.integer "price", null: false
     t.text "description", null: false
+    t.bigint "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["user_id"], name: "index_items_on_user_id"
